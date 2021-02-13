dcdf9b319bc4f142ce06b84ea19a56dfa7157500
marzelwidmer <marzelwidmer@gmail.com>
2021-02-13 17:01:56 +0100

    Work on Gateway Restdocs Snippets

--- gateway-service/src/test/kotlin/ch/keepcalm/demo/base/BaseClass.kt
+++ gateway-service/src/test/kotlin/ch/keepcalm/demo/base/BaseClass.kt
@@ -1,25 +1,40 @@
 package ch.keepcalm.demo.base
 
 import ch.keepcalm.demo.GatewayService
-import io.restassured.RestAssured
+import ch.keepcalm.demo.index.IndexResource
+import io.restassured.module.webtestclient.RestAssuredWebTestClient
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.web.server.LocalServerPort
+import org.springframework.restdocs.RestDocumentationContextProvider
 import org.springframework.restdocs.RestDocumentationExtension
+import org.springframework.restdocs.operation.preprocess.Preprocessors
+import org.springframework.restdocs.webtestclient.WebTestClientRestDocumentation.documentationConfiguration
 import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.test.web.reactive.server.WebTestClient
+
 
 @SpringBootTest(classes = [GatewayService::class], webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT, properties = ["server.port=8080"])
 @AutoConfigureRestDocs(outputDir = "build/generated-snippets")
 @ExtendWith(RestDocumentationExtension::class, SpringExtension::class)
 abstract class BaseClass {
-    @LocalServerPort
-    var port = 0
+
     @BeforeEach
-    fun setup() {
-//        RestAssured.baseURI = "http://localhost:" + port
-//        RestAssured.baseURI = "http://localhost:8080"
-        RestAssured.basePath = "/"
+    fun setup(restDocumentation: RestDocumentationContextProvider?) {
+        RestAssuredWebTestClient.webTestClient(
+            WebTestClient.bindToController(
+                IndexResource::class
+            ).configureClient()
+                .filter(
+                    documentationConfiguration(restDocumentation)
+                        .snippets()
+                        .and()
+                        .operationPreprocessors()
+                        .withRequestDefaults(Preprocessors.prettyPrint())
+                        .withResponseDefaults(Preprocessors.prettyPrint())
+                )
+                .build()
+        )
     }
 }
